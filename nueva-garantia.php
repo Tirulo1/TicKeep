9080e99417f6fc5d0b4db4887b71cb4c2bea213e
Rober-06 <robertotirulo@gmail.com>
2026-05-15 00:21:35 +0200

    estilos header

--- nueva-garantia.php
+++ nueva-garantia.php
@@ -181,6 +181,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/preferencias.css">
    <link rel="stylesheet" href="assets/css/nueva-garantia.css">
+   <link rel="stylesheet" href="assets/css/auth.css">
 </head>
 
 <body>
